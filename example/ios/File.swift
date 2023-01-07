494dbec9190e58699a9619cad16303b254410903
Kouta Imanaka <keima@p-side.net>
2023-01-07 20:51:11 +0900

    import from original one

---
+++ example/ios/File.swift
@@ -0,0 +1,6 @@
+//
+//  File.swift
+//  YoutubePlayerNativeExample
+//
+
+import Foundation
---
+++ src/icons/index.tsx
@@ -0,0 +1,39 @@
+import React from 'react'
+import { TouchableOpacity, Image, StyleSheet } from 'react-native'
+
+const PlayImage = require('./play.png')
+const PauseImage = require('./pause.png')
+const FSImage = require('./fullscreen.png')
+const ExitFSImage = require('./fullscreen-exit.png')
+
+type Props = {
+  onPress: () => void
+  size?: number
+}
+
+export const PlayIcon = ({ onPress, size = 40 }: Props) => (
+  <TouchableOpacity style={styles.container} onPress={onPress}>
+    <Image style={{ height: size, width: size }} source={PlayImage} />
+  </TouchableOpacity>
+)
+export const PauseIcon = ({ onPress, size = 40 }: Props) => (
+  <TouchableOpacity style={styles.container} onPress={onPress}>
+    <Image style={{ height: size, width: size }} source={PauseImage} />
+  </TouchableOpacity>
+)
+export const FSIcon = ({ onPress, size = 40 }: Props) => (
+  <TouchableOpacity style={styles.container} onPress={onPress}>
+    <Image style={{ height: size, width: size }} source={FSImage} />
+  </TouchableOpacity>
+)
+export const ExitFSIcon = ({ onPress, size = 40 }: Props) => (
+  <TouchableOpacity style={styles.container} onPress={onPress}>
+    <Image style={{ height: size, width: size }} source={ExitFSImage} />
+  </TouchableOpacity>
+)
+
+const styles = StyleSheet.create({
+  container: {
+    padding: 5,
+  },
+})
